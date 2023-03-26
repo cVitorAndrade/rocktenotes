30d161affb4242c04797ffaa01a1b4b3e2584d8f
cVitorAndrade <vitorandrade1804@gmail.com>
2023-03-26 16:48:54 -0300

    feat(utils): adding AppError class for future exception handling

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,5 @@
 class UsersController {
-    create(request, response) {
+    create (request, response) {
         const { name, email, password } = request.body;
 
         response.json({ name, email, password });
