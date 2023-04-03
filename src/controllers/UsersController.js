cd4f6d815fcb15341a50c4ef0992d0c81e017894
cVitorAndrade <vitorandrade1804@gmail.com>
2023-04-03 16:36:22 -0300

    feat(UsersController): implement update method

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const sqliteConnection = require("../database/sqlite");
 const AppError = require("../utils/AppError");
 
@@ -20,6 +20,47 @@ class UsersController {
             );
 
         response.status(201).json({});
+    }
+
+    async update ( request, response ) {
+        const { name, email, password, old_password } = request.body;
+        const { id } = request.params;
+        const database = await sqliteConnection();
+
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+        if ( !user ) {
+            throw new AppError("Usuário não encontrado.");
+        }
+
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = ?", [email]);
+        if ( userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id ) {
+            throw new AppError("Email já cadastrado.");
+        }
+
+        user.name = (name !== null && name !== undefined) ? name : user.name;
+        user.email = (email !== null && email !== undefined) ? email : user.email;
+
+        if ( password && !old_password ) {
+            throw new AppError("É necessário informar a senha antiga.");
+        }
+        if ( password && old_password ) {
+            const checkOldPassword = await compare(old_password, user.password);
+            if ( !checkOldPassword ) {
+                throw new AppError("A senha antiga não coincide com a informada.");
+            }
+            user.password = await hash(password, 8);
+        }
+
+        await database.run(`
+                UPDATE users SET
+                name = ?,
+                email = ?,
+                password = ?,
+                updated_at = DATETIME('now')
+                WHERE id = ?`,
+                [user.name, user.email, user.password, id]);
+            
+        response.json();
     }
 }
 
